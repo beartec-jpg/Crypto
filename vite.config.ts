a5653188ecfac49c2fdbc22f9ac55bea39d940b5
beartec-jpg <beartec@beartec.uk>
2025-11-28 11:29:19 +0000

    Change build output directory and enable emptyOutDir
    Updated build output directory to '../dist' and set emptyOutDir to true.

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: 'client',
   base: '/',
   build: {
-    outDir: 'dist',
+    outDir: '../dist', // Builds from client/ to root/dist
+    emptyOutDir: true,
     sourcemap: false
   },
   server: {
