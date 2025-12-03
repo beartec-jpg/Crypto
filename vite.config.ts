4c19ba4a9dbbf36ef7770ac5f29879d83254a178
beartec-jpg <beartec@beartec.uk>
2025-12-03 08:58:10 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  pluginsOLER: [react()],
+  plugins: [react()],
 
   // This tells Vite where your React app lives
   root: 'client',
